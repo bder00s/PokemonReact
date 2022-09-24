4fe1fc79d021d23ad95522fb81ea8a74c955c436
bder00s <bonne-deroos@hotmail.com>
2022-09-24 14:16:28 +0200

    eerste opzet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+    async function getData(){
+        try {
+           const result = await axios.get('')
+        }
+    }
+
   return (
     <div>
       Begin hier met de opdracht!
