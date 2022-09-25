cb444da20d14a237d95280c72de5ad3250a070a9
bder00s <bonne-deroos@hotmail.com>
2022-09-25 17:46:25 +0200

    opzet van de async functie, useState en useEffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
+import {useState} from "react";
+
+
 
 function App() {
-    async function getData(){
-        try {
-           const result = await axios.get('')
-        }
-    }
+    const [pokemon, setPokemon] = useState('');
+    useEffect(() => {
 
-  return (
-    <div>
-      Begin hier met de opdracht!
-    </div>
-  );
+        async function getData() {
+            try {
+                const result = await axios.get(' https://pokeapi.co/api/v2/pokemon/jigglypuff');
+                console.log(result.data);
+                setPokemon(result.data);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        getData()
+    }, []);
+    return (
+        Object.keys(setPokemon).length > 0 &&
+                <>
+                    <article>
+                        <h1>{pokemon.name}</h1>
+                       <p>{pokemon.types.type[0]}</p>
+                    </article>
+                </>
+
+
+    );
 }
 
 export default App;
+
+
+
+// .map((poke) => {
+//     <ol>
+//         <li>{poke}</li>
+//     </ol>
+// });
