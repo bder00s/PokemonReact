d9b4d8450051f1b046aa722a68fa79c602655a53
bder00s <bonne-deroos@hotmail.com>
2022-09-26 16:29:00 +0200

    Pokemon kaart werkend!

--- src/App.js
+++ src/App.js
@@ -24,16 +24,17 @@ function App() {
 
     return (
         <div>
-            {setPokemon.length > 0 &&
+            {pokemon &&
                 <article>
                     <h1>{pokemon.name}</h1>
-                    <img src={pokemon.sprites.front_default}/>
+                    <img src={pokemon.sprites.front_default} alt="pokemon"/>
                     <p>Moves: {pokemon.moves.length}</p>
                     <p>Weight: {pokemon.weight} </p>
                     <p>Abilities:</p>
-                    <button type="button" id="ability-1"
-                    >{pokemon.abilities[0].ability.name}</button>
+                    <button type="button" id="ability-1">{pokemon.abilities[0].ability.name}</button>
                     <button type="button" id="ability-2">{pokemon.abilities[1].ability.name}</button>
+                    {pokemon.abilities.length > 2 &&
+                        <button type="button" id="ability-3">{pokemon.abilities[2].ability.name}</button>}
 
                 </article>
             }
