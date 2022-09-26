aea0705887400da9a6a0002efe443222564d7f26
bder00s <bonne-deroos@hotmail.com>
2022-09-26 15:32:27 +0200

    Pokemonkaartje voor één pokemon

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
-import React, {useEffect} from 'react';
+import React, {useEffect} from "react";
 import './App.css';
-import axios from 'axios';
-import {useState} from "react";
-
-
+import axios from 'axios'
 
 function App() {
-    const [pokemon, setPokemon] = useState('');
+    const [pokemon, setPokemon] = React.useState('');
+
     useEffect(() => {
 
         async function getData() {
             try {
-                const result = await axios.get(' https://pokeapi.co/api/v2/pokemon/jigglypuff');
+                const result = await axios.get('https://pokeapi.co/api/v2/pokemon/jigglypuff');
                 console.log(result.data);
                 setPokemon(result.data);
-            } catch (e) {
-                console.error(e);
+
+            } catch (error) {
+                console.error(error);
             }
         }
+
         getData()
+
     }, []);
+
     return (
-        Object.keys(setPokemon).length > 0 &&
-                <>
-                    <article>
-                        <h1>{pokemon.name}</h1>
-                       <p>{pokemon.types.type[0]}</p>
-                    </article>
-                </>
+        <div>
+            {setPokemon.length > 0 &&
+                <article>
+                    <h1>{pokemon.name}</h1>
+                    <img src={pokemon.sprites.front_default}/>
+                    <p>Moves: {pokemon.moves.length}</p>
+                    <p>Weight: {pokemon.weight} </p>
+                    <p>Abilities:</p>
+                    <button type="button" id="ability-1"
+                    >{pokemon.abilities[0].ability.name}</button>
+                    <button type="button" id="ability-2">{pokemon.abilities[1].ability.name}</button>
+
+                </article>
+            }
+
+        </div>
 
 
-    );
+    )
+
+
 }
 
-export default App;
-
-
-
-// .map((poke) => {
-//     <ol>
-//         <li>{poke}</li>
-//     </ol>
-// });
+export default App
